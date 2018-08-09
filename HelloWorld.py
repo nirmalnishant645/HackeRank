7fa03d804504288bb7c1eaeb2fe3e9799e3a85dc
Nishant Nirmal <32808636+nirmalnishant645@users.noreply.github.com>
2018-08-09 23:44:33 +0530

    Create HelloWorld.py

---
+++ HelloWorld.py
@@ -0,0 +1,6 @@
+# Read a full line of input from stdin and save it to our dynamically typed variable, input_string.
+input_string = input()
+
+# Print a string literal saying "Hello, World." to stdout.
+print('Hello, World.')
+print (input_string);
