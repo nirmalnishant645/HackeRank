887688f338bae27f7dc5aff68f33ac796da8d6de
Nishant Nirmal <32808636+nirmalnishant645@users.noreply.github.com>
2018-08-13 21:15:19 +0530

    Update If-Else.py

--- If-Else.py
+++ If-Else.py
@@ -1,16 +1,17 @@
 '''
 Task 
-Given an integer, , perform the following conditional actions:
+Given an integer,n, perform the following conditional actions:
 
-If  is odd, print Weird
-If  is even and in the inclusive range of  to , print Not Weird
-If  is even and in the inclusive range of  to , print Weird
-If  is even and greater than , print Not Weird
+If n is odd, print Weird
+If n is even and in the inclusive range of 2 to 5, print Not Weird
+If n is even and in the inclusive range of 6 to 20, print Weird
+If n is even and greater than 20, print Not Weird
 Input Format
 
-A single line containing a positive integer, .
+A single line containing a positive integer, n.
 
 Constraints
+1<=n<=100
 
 Output Format
 
