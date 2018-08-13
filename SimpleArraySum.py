1142c227db7cf995fb3656b6d5551c048aa1f74c
Nishant Nirmal <32808636+nirmalnishant645@users.noreply.github.com>
2018-08-13 21:12:50 +0530

    Update SimpleArraySum.py

--- SimpleArraySum.py
+++ SimpleArraySum.py
@@ -1,4 +1,28 @@
-def solveMeFirst(a,b):
+'''
+Complete the function solveMeFirst to compute the sum of two integers.
+
+Function prototype:
+
+int solveMeFirst(int a, int b);
+
+where,
+
+a is the first integer input.
+b is the second integer input
+Return values
+
+sum of the above two integers
+Sample Input
+
+a = 2
+b = 3
+Sample Output
+
+5
+Explanation
+
+The sum of the two integers a and b is computed as: 2+3=5.
+'''def solveMeFirst(a,b):
 	# Hint: Type return a+b below
     return a+b
 
