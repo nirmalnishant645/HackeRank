e5536d8af8d407d425e0b629b1858ab370f66ba2
Nishant Nirmal <32808636+nirmalnishant645@users.noreply.github.com>
2018-08-25 12:07:09 +0530

    Update Functions.py

--- Functions.py
+++ Functions.py
@@ -7,18 +7,30 @@ The year can be evenly divided by 100, it is NOT a leap year, unless:
 The year is also evenly divisible by 400. Then it is a leap year.
 This means that in the Gregorian calendar, the years 2000 and 2400 are leap years, while 1800, 1900, 2100, 2200, 
 2300 and 2500 are NOT leap years
+
 Task 
+
 You are given the year, and you have to write a function to check if the year is leap or not.
 Note that you have to complete the function and remaining code is given as template.
+
 Input Format
+
 Read y, the year that needs to be checked.
+
 Output Format
+
 Output is taken care of by the template. Your function must return a boolean value (True/False)
+
 Sample Input:
+
 1990
+
 Sample Output:
+
 False
+
 Explanation:
+
 1990 is not a multiple of 4 hence it's not a leap year.
 """
 def is_leap(year):
