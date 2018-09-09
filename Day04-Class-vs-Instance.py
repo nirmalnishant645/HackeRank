deebf72c3a0d158ce07a8f126d01b19d86e899f7
Nishant Nirmal <nirmalnishant645@gmail.com>
2018-09-09 13:18:42 +0530

    Update Day04-Class-vs-Instance.py

--- Day04-Class-vs-Instance.py
+++ Day04-Class-vs-Instance.py
@@ -15,6 +15,53 @@ Input Format
 Input is handled for you by the stub code in the editor.
 
 The first line contains an integer, T (the number of test cases), and the T subsequent lines each contain an integer denoting the age of a Person instance.
+
+Constraints
+1 <= T <= 4
+-5 <= age <= 30
+
+Output Format
+Complete the method definitions provided in the editor so they meet the specifications outlined above; the code to test your work is already in the editor. If your methods are implemented correctly, each test case will print 2 or 3 lines (depending on whether or not a valid initialAge was passed to the constructor).
+
+Sample Input 0
+4
+-1
+10
+16
+18
+
+Sample Output 0
+Age is not valid, setting age to 0.
+You are young.
+You are young.
+
+You are young.
+You are a teenager.
+
+You are a teenager.
+You are old.
+
+You are old.
+You are old.
+
+Explanation
+Test Case 0: initialAge = -1
+
+Because initialAge < 0, our code must set age to 0 and print the "Age is not valid.." message followed by the young message. Three years pass and age = 3, so we print the young message again.
+
+Test Case 1: initialAge = 10
+
+Because initialAge < 13, our code should print that the person is young. Three years pass and age = 13, so we print that the person is now a teenager.
+
+Test Case 2: initialAge = 16
+
+Because , 13 <= initialAge < 18, our code should print that the person is a teenager. Three years pass and age = 19, so we print that the person is old.
+
+Test Case 3: initialAge = 18
+
+Because initialAge >= 18, our code should print that the person is old. Three years pass and the person is still old at age = 21, so we print the old message again.
+
+The extra line at the end of the output is supposed to be there and is trimmed before being compared against the test case's expected output. If you're failing this challenge, check your logic and review your print statements for spelling errors.
 '''
 class Person:
     def __init__(self,initialAge):
