4e16c68ab363eddc1ceae8bbb32100cefa9628fc
Nishant Nirmal <nirmalnishant645@gmail.com>
2018-09-09 13:14:35 +0530

    Update Day01-Data-Types.py

--- Day01-Data-Types.py
+++ Day01-Data-Types.py
@@ -18,6 +18,25 @@ The third line contains a string that you must concatenate with s.
 Output Format
 
 Print the sum of both integers on the first line, the sum of both doubles (scaled to 1 decimal place) on the second line, and then the two concatenated strings on the third line.
+
+Sample Input
+12
+4.0
+is the best place to learn and practice coding!
+
+Sample Output
+16
+8.0
+HackerRank is the best place to learn and practice coding!
+
+Explanation
+When we sum the integers 4 and 12, we get the integer 16.
+
+When we sum the floating point numbers 4.0 and 4.0, we get 8.0.
+
+When we concatenate HackerRank with is the best place to learn and practice coding!, we get HackerRank is the best place to learn and practice coding!.
+
+You will not pass this challenge if you attempt to assign the Sample Case values to your variables instead of following the instructions above and reading input from stdin.
 '''
 
 i = 4
