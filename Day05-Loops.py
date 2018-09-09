428c866f1a5b60486b6c66bb640cfe23b4302506
Nishant Nirmal <nirmalnishant645@gmail.com>
2018-09-09 13:19:29 +0530

    Update Day05-Loops.py

--- Day05-Loops.py
+++ Day05-Loops.py
@@ -13,7 +13,23 @@ Output Format
 
 Print 10 lines of output; each line i (where 1<=i<=10) contains the result of n x i in the form: 
 n x i = result.
+
+Sample Input
+2
+
+Sample Output
+2 x 1 = 2
+2 x 2 = 4
+2 x 3 = 6
+2 x 4 = 8
+2 x 5 = 10
+2 x 6 = 12
+2 x 7 = 14
+2 x 8 = 16
+2 x 9 = 18
+2 x 10 = 20
 '''
+
 import sys
 
 n = int(input().strip())
