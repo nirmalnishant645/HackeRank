15e642abe4887a8ae58380bf553c30930bedf27d
Nishant Nirmal <nirmalnishant645@gmail.com>
2018-09-10 09:58:35 +0530

    Update Day14-Scope.py

--- Day14-Scope.py
+++ Day14-Scope.py
@@ -18,6 +18,23 @@ Constraints
 Output Format
 
 You are not responsible for printing any output; the Solution class will print the value of the maximumDifference instance variable.
+
+Sample Input
+3
+1 2 5
+
+Sample Output
+4
+
+Explanation
+The scope of the elements array and maximumDifference integer is the entire class instance. The class constructor saves the argument passed to the constructor as the elements instance variable (where the computeDifference method can access it).
+
+To find the maximum difference, computeDifference checks each element in the array and finds the maximum difference between any 2 elements:
+|1 - 2| = 1
+|1 - 5| = 4
+|2 - 5| = 3
+
+The maximum of these differences is 4, so it saves the value 4 as the maximumDifference instance variable. The locked stub code in the editor then prints the value stored as maximumDifference, which is 4.
 '''
 
 class Difference:
