5339ee1a140f85300830ecf376a406f3deab296d
Nishant Nirmal <nirmalnishant645@gmail.com>
2018-09-10 09:52:27 +0530

    Update Day10-Binary-Numbers.py

--- Day10-Binary-Numbers.py
+++ Day10-Binary-Numbers.py
@@ -13,6 +13,23 @@ Constraints
 Output Format
 
 Print a single base-10 integer denoting the maximum number of consecutive 1's in the binary representation of n.
+
+Sample Input 1
+5 
+Sample Output 1
+1 
+
+Sample Input 2
+13
+Sample Output 2
+2
+
+Explanation
+Sample Case 1:
+The binary representation of 5 is 101, so the maximum number of consecutive 1's is 1.
+
+Sample Case 2:
+The binary representation of 13 is 1101, so the maximum number of consecutive 1's is 2.
 '''
 
 #!/bin/python3
