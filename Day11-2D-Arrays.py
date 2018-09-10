69d7d47c8c7afd8b41b5128152addf975c1f2b63
Nishant Nirmal <nirmalnishant645@gmail.com>
2018-09-10 09:53:30 +0530

    Update Day11-2D-Arrays.py

--- Day11-2D-Arrays.py
+++ Day11-2D-Arrays.py
@@ -15,6 +15,42 @@ Constraints
 Output Format
 
 Print the largest (maximum) hourglass sum found in A.
+
+Sample Input
+1 1 1 0 0 0
+0 1 0 0 0 0
+1 1 1 0 0 0
+0 0 2 4 4 0
+0 0 0 2 0 0
+0 0 1 2 4 0
+
+Sample Output
+19
+
+Explanation
+A contains the following hourglasses:
+
+1 1 1   1 1 0   1 0 0   0 0 0
+  1       0       0       0
+1 1 1   1 1 0   1 0 0   0 0 0
+
+0 1 0   1 0 0   0 0 0   0 0 0
+  1       1       0       0
+0 0 2   0 2 4   2 4 4   4 4 0
+
+1 1 1   1 1 0   1 0 0   0 0 0
+  0       2       4       4
+0 0 0   0 0 2   0 2 0   2 0 0
+
+0 0 2   0 2 4   2 4 4   4 4 0
+  0       0       2       0
+0 0 1   0 1 2   1 2 4   2 4 0
+
+The hourglass with the maximum sum (19) is:
+
+2 4 4
+  2
+1 2 4
 '''
 
 #!/bin/python3
