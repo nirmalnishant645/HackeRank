d6cb75780b4ac8fd662b546a57a60b8e02181595
Nishant Nirmal <nirmalnishant645@gmail.com>
2018-09-10 09:59:21 +0530

    Update Day15-Linked-List.py

--- Day15-Linked-List.py
+++ Day15-Linked-List.py
@@ -14,6 +14,20 @@ You do not need to read anything from stdin.
 Output Format
 
 Your insert function should return a reference to the head node of the linked list.
+
+Sample Input
+The following input is handled for you by the locked code in the editor: The first line contains T, the number of test cases. The T subsequent lines of test cases each contain an integer to be inserted at the list's tail.
+
+4
+2
+3
+4
+1
+
+Sample Output
+The locked code in your editor prints the ordered data values for each element in your list as a single line of space-separated integers:
+
+2 3 4 1
 '''
 
 class Node:
