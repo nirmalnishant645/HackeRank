31b0bf4b15da01a50624c0627e974e0441222c40
Nishant Nirmal <nirmalnishant645@gmail.com>
2018-09-10 10:00:11 +0530

    Update Day16-Exceptions-String-to-Integer.py

--- Day16-Exceptions-String-to-Integer.py
+++ Day16-Exceptions-String-to-Integer.py
@@ -15,6 +15,20 @@ S is composed of either lowercase letters (a-z) or decimal digits (0-9).
 Output Format
 
 Print the parsed integer value of S, or Bad String if S cannot be converted to an integer.
+
+Sample Input 0
+3
+Sample Output 0
+3
+
+Sample Input 1
+za
+Sample Output 1
+Bad String
+
+Explanation
+Sample Case 0 contains an integer, so it should not raise an exception when we attempt to convert it to an integer. Thus, we print the 3.
+Sample Case 1 does not contain any integers, so an attempt ot convert it to an integer will raise an exception. Thus, our exception handler prints Bad String.
 '''
 
 #!/bin/python3
