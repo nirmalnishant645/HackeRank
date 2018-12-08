db2f56d5e3e3f8e60b5012e73a60c1e7f8efc9aa
Nishant Nirmal <nirmalnishant645@gmail.com>
2018-12-08 19:08:45 +0530

    Generics

--- Cpp/30-Days-of-Code/Day21-Generics.cpp
+++ Cpp/30-Days-of-Code/Day21-Generics.cpp
@@ -16,6 +16,39 @@ Your printArray function should print each element of its generic array paramete
 #include <iostream>
 #include <vector>
 #include <string>
+using namespace std;
+template <typename T> void printArray(vector<T> a) {
+  for (T i : a)
+    cout << i << endl;
+}
+int main() {
+	int n;
+	
+	cin >> n;
+	vector<int> int_vector(n);
+	for (int i = 0; i < n; i++) {
+		int value;
+		cin >> value;
+		int_vector[i] = value;
+	}
+	
+	cin >> n;
+	vector<string> string_vector(n);
+	for (int i = 0; i < n; i++) {
+		string value;
+		cin >> value;
+		string_vector[i] = value;
+	}
+	printArray<int>(int_vector);
+	printArray<string>(string_vector);
+	return 0;
+}
+
+/*
+
+#include <iostream>
+#include <vector>
+#include <string>
 
 using namespace std;
 template <typename t>
@@ -48,3 +81,5 @@ int main() {
 
 	return 0;
 }
+
+*/
