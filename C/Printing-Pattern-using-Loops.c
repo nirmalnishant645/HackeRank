c952200deae2b1d95f79b7f200c4967eb6b41c36
Nishant Nirmal <nirmalnishant645@gmail.com>
2019-09-23 08:44:21 +0530

    Printing Pattern using Loops

---
+++ C/Printing-Pattern-using-Loops.c
@@ -0,0 +1,94 @@
+/*
+Task
+In this problem, you need to print the pattern of the following form containing the numbers from 1 to n.
+
+                            4 4 4 4 4 4 4  
+                            4 3 3 3 3 3 4   
+                            4 3 2 2 2 3 4   
+                            4 3 2 1 2 3 4   
+                            4 3 2 2 2 3 4   
+                            4 3 3 3 3 3 4   
+                            4 4 4 4 4 4 4   
+Input Format
+
+The input will contain a single integer n.
+
+Constraints
+
+1<=n<=1000
+
+Output Format
+
+Print the pattern mentioned in the problem statement.
+
+Sample Input 0
+
+2
+
+Sample Output 0
+
+2 2 2
+2 1 2
+2 2 2
+
+Sample Input 1
+
+5
+
+Sample Output 1
+
+5 5 5 5 5 5 5 5 5 
+5 4 4 4 4 4 4 4 5 
+5 4 3 3 3 3 3 4 5 
+5 4 3 2 2 2 3 4 5 
+5 4 3 2 1 2 3 4 5 
+5 4 3 2 2 2 3 4 5 
+5 4 3 3 3 3 3 4 5 
+5 4 4 4 4 4 4 4 5 
+5 5 5 5 5 5 5 5 5
+
+Sample Input 2
+
+7
+
+Sample Output 2
+
+7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 6 6 6 6 6 6 6 6 6 6 6 7 
+7 6 5 5 5 5 5 5 5 5 5 6 7 
+7 6 5 4 4 4 4 4 4 4 5 6 7 
+7 6 5 4 3 3 3 3 3 4 5 6 7 
+7 6 5 4 3 2 2 2 3 4 5 6 7 
+7 6 5 4 3 2 1 2 3 4 5 6 7 
+7 6 5 4 3 2 2 2 3 4 5 6 7 
+7 6 5 4 3 3 3 3 3 4 5 6 7 
+7 6 5 4 4 4 4 4 4 4 5 6 7 
+7 6 5 5 5 5 5 5 5 5 5 6 7 
+7 6 6 6 6 6 6 6 6 6 6 6 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 
+*/
+#include <stdio.h>
+
+int main() 
+{
+
+    int i, j, n, m;
+    scanf("%d", &n);
+  	
+    for (i=0;i<2*n-1;i++) {
+
+        for (j=0;j<2*n-1;j++) {
+
+            m = (i<j?i:j) < (2*n-1-i) ? (i<j?i:j) : (2*n-2-i);
+            m = m < (2*n-2-j) ? m : (2*n-2-j); 
+
+            printf("%d ",n-m);
+
+        }
+
+        printf("\n");
+
+    }
+
+    return 0;
+}
