cb8a51f8338b3e14ee3f53d0dc98124c56f7ab9d
nirmalnishant645 <nirmalnishant645@gmail.com>
2020-10-31 21:53:55 +0530

    Reverse Array Iterative Solution

--- Python/Reverse-Array.py
+++ Python/Reverse-Array.py
@@ -21,3 +21,32 @@ start = start + 1, end = end - 1
 
 Time Complexity : O(n)
 '''
+# Iterative python program to reverse an array
+ 
+# Function to reverse A[] from start to end
+def reverseList(A):
+    start = 0
+    end = len(A) - 1
+    while start < end:
+        A[start], A[end] = A[end], A[start]
+        start += 1
+        end -= 1
+ 
+# Driver function to test above function
+A = [1, 2, 3, 4, 5, 6]
+print('Iterative Method:\n', A)
+reverseList(A)
+print("Reversed list is: ")
+print(A)
+
+'''
+Recursive Way :
+ 
+
+1) Initialize start and end indexes as start = 0, end = n - 1 
+2) Swap arr[start] with arr[end] 
+3) Recursively call reverse for rest of the array.
+
+Time Complexity : O(n)
+'''
+
