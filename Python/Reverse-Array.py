c739e9dec5e83406f315599267c28af0ea74eea7
nirmalnishant645 <nirmalnishant645@gmail.com>
2020-10-31 21:54:12 +0530

    Reverse Array Recursive Solution

--- Python/Reverse-Array.py
+++ Python/Reverse-Array.py
@@ -50,3 +50,31 @@ Recursive Way :
 Time Complexity : O(n)
 '''
 
+# Recursive python program to reverse an array
+ 
+# Function to reverse A[] from start to end
+def reverseList(A, start, end):
+    if start >= end:
+        return
+    A[start], A[end] = A[end], A[start]
+    reverseList(A, start+1, end-1)
+ 
+# Driver function to test above function
+A = [1, 2, 3, 4, 5, 6]
+print('Recursive Method:\n', A)
+reverseList(A, 0, len(A) - 1)
+print("Reversed list is: ")
+print(A)
+
+'''
+Another Approach: Using Python List slicing
+'''
+
+def reverseList(A):
+  print( A[::-1])
+     
+# Driver function to test above function
+A = [1, 2, 3, 4, 5, 6]
+print('List Slicing Method:\n', A)
+print("Reversed list is: ")
+reverseList(A) 
