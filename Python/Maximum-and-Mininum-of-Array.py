1a676c4cf32a245a7e22ca060814c742ccb48231
nirmalnishant645 <nirmalnishant645@gmail.com>
2020-11-01 22:51:47 +0530

    Maximum Minimum Tournament Method

--- Python/Maximum-and-Mininum-of-Array.py
+++ Python/Maximum-and-Mininum-of-Array.py
@@ -53,3 +53,64 @@ if __name__ == "__main__":
     print('Simple Linear Search: ')
     print("Minimum element is: ", minmax.min)
     print("Maximum element is: ", minmax.max)
+
+
+
+'''
+METHOD 2 (Tournament Method) 
+Divide the array into two parts and compare the maximums and minimums of the two parts to get the maximum and the minimum of the whole array.
+
+Pair MaxMin(array, array_size)
+   if array_size = 1
+      return element as both max and min
+   else if arry_size = 2
+      one comparison to determine max and min
+      return that pair
+   else    /* array_size  > 2 */
+      recur for max and min of left half
+      recur for max and min of right half
+      one comparison determines true max of the two candidates
+      one comparison determines true min of the two candidates
+      return the pair of max and min
+
+Time Complexity: O(n) 
+'''
+
+# Python program of above implementation
+def getRecMinMax(low, high, arr):
+    arr_max = arr[low]
+    arr_min = arr[low]
+     
+    # If there is only one element
+    if low == high:
+        arr_max = arr[low]
+        arr_min = arr[low]
+        return (arr_max, arr_min)
+         
+    # If there is only two element
+    elif high == low + 1:
+        if arr[low] > arr[high]:
+            arr_max = arr[low]
+            arr_min = arr[high]
+        else:
+            arr_max = arr[high]
+            arr_min = arr[low]
+        return (arr_max, arr_min)
+    else:
+         
+        # If there are more than 2 elements
+        mid = int((low + high) / 2)
+        arr_max1, arr_min1 = getRecMinMax(low, mid, arr)
+        arr_max2, arr_min2 = getRecMinMax(mid + 1, high, arr)
+ 
+    return (max(arr_max1, arr_max2), min(arr_min1, arr_min2))
+ 
+# Driver code
+arr = [1000, 11, 445, 1, 330, 3000]
+high = len(arr) - 1
+low = 0
+print('Tournament Method: ')
+arr_max, arr_min = getRecMinMax(low, high, arr)
+print('Minimum element is: ', arr_min)
+print('nMaximum element is: ', arr_max)
+
